f2979b7a545d1c00f609434366ba7c001c742bc0
Tkachuk Pavel <pavelt0309@gmail.com>
2016-11-29 14:10:15 +0300

    lab 4

---
+++ lab4/lab4.py
@@ -0,0 +1,39 @@
+import numpy as np
+import numpy.linalg as linalg
+
+
+# Метод отражений
+
+
+def solve(A, f):
+    size = len(f)
+    E = np.eye(size)
+    for i in range(size - 1):
+        s = np.array([0 if j < i else A[j, i] for j in range(size)])
+        e = E[:, i]
+        a = np.sqrt(np.dot(s, s))
+        k = 1/ np.sqrt(2 * np.dot(s, s - a * e))
+        w = np.array(k * (s - a * e))
+        V = np.array([[E[i, j] - 2 * w[i] * w[j] for j in range(size)] for i in range(size)])
+        A = np.dot(V, A)
+        f = np.dot(V, f)
+    x = np.zeros(size)
+    for i in reversed(range(size)):
+        x[i] = (f[i] - sum(A[i, j] * x[j] for j in range(i + 1, size))) / A[i, i]
+    return x
+
+
+file = open("matrix", "r")  # Чтение файла
+A, b = [], []
+for line in file:
+    A.append([float(el) for el in line.split()[:-1]])
+    b.append(float(line.split().pop()))
+A = np.array(A)
+b = np.array(b)
+print("Решаем матричное уравнение методом квадратного корня")
+print("Основная матрица системы:")
+print(A)
+print("Свободные члены: ", b)
+ans = solve(A, b)
+print("Решение системы: ", ans)
+print("Невязка: ", linalg.norm(np.dot(A, ans) - b))
